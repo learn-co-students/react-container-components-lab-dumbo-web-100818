7c872e78ea9a8fccba9af1bfbe636aaedb2a4bea
said shah <saidmysr20@aol.com>
2018-12-18 13:05:26 -0500

    added movie reviews component

--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -7,3 +7,15 @@ const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
             + `api-key=${NYT_API_KEY}`;
 
 // Code SearchableMovieReviewsContainer Here
+
+class SearchableMovieReviewsContainer extends Component {
+
+  render() {
+    return (
+      <div className="searchable-movie-reviews"></div>
+    );
+  }
+
+}
+
+export default SearchableMovieReviewsContainer;
